0bf14afca3d6b0d73efcc0919aaf4d84125194f1
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-02-26 18:37:24 +0330

    Update tupleunpackingtest.py

--- tupleunpackingtest.py
+++ tupleunpackingtest.py
@@ -1,1 +1,9 @@
-print('hallo wirklich')
+def calc(x):
+    #your code goes here
+    return (x*4, x**2)
+
+side = int(input())
+p, a = calc(side)
+
+print("Perimeter: " + str(p))
+print("Area: " + str(a))

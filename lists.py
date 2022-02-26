412f1ff1f495c07b0cacfd571e0289c0ab039911
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-02-26 14:47:58 +0330

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,11 @@
+data = {
+    'Singapore': 1,
+    'Ireland': 6,
+    'United Kingdom': 7,
+    'Germany': 27,
+    'Armenia': 34,
+    'United States': 17,
+    'Canada': 9,
+    'Italy': 74
+}
+print (data.get(input(),"Not found"))

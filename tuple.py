4a172f52d9030a2eb0f34eae0acf2fce2bfd6714
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-02-26 15:07:31 +0330

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,15 @@
+contacts = [
+    ('James', 42),
+    ('Amy', 24),
+    ('John', 31),
+    ('Amanda', 63),
+    ('Bob', 18)
+]
+dic = dict(contacts)
+print(dic)
+name = 'roja'
+age=dic.get(name, 'Not found')
+if age != 'Not found':
+    print(name+' is '+str(age))
+else:
+    print('Not found')

74f909254419a06ddaf791e436d360c279f67677
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-02-27 11:20:53 +0330

    Update anonms_func.py

--- functionalprgrmng/anonms_func.py
+++ functionalprgrmng/anonms_func.py
@@ -3,4 +3,22 @@ def  my_func(f, arg):
 a= my_func(lambda x: 2*x*x, 5)
 print(a)
 b= (lambda x: x*x)(8)
-print(b)
+print(b) 
+
+"""map 
+&
+filter"""
+
+def add_five(x):
+	return x+5
+nums = [11, 22, 33, 44, 55]
+result = list(map(add_five, nums))
+print(result)
+
+nums = [11, 22, 33, 44, 55]
+result = list(map(lambda x: x+5, nums))
+print(result)
+
+nums = [11, 22, 33]
+a = list(map(lambda x: x*2, nums))
+print(a)

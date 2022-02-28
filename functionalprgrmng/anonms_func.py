27fba24707cd9102471a7c10542d9bd815db8570
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-02-28 13:40:33 +0330

    Update anonms_func.py

--- functionalprgrmng/anonms_func.py
+++ functionalprgrmng/anonms_func.py
@@ -35,4 +35,17 @@ def countdown():
 		i -= 1
 for i in countdown():
 	print (i)
-print(countdown())
+
+def numbers (x):
+	for i in range(x):
+		if i % 2 ==0:
+			yield i
+
+print(list(numbers(11)))
+
+def make_word():
+	word=""
+	for ch in "spam":
+		word += ch
+		yield word
+print(list(make_word()))

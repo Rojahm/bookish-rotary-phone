430f6b04c8cd13986588e836c724dbe4d7bcde74
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-03-02 16:01:11 +0330

    Update oop.py

--- oop/oop.py
+++ oop/oop.py
@@ -6,9 +6,35 @@ we can use a class to make multiple objcs"""
 
 class Cat:
 #these are called class methods(which are funcs)
+#init is called when an instance of
+#an obj of the class is created,
+#using the class name as function
 	def __init__(self, color, legs):
+		#color and legs are attributes of cat instan
 		self.color = color
 		self.legs = legs
 felix = Cat("ginger", 4)
 rover = Cat("dog-colored", 4)
 stumpy = Cat("brown", 3)
+
+print(rover.color)
+print("-----------")
+class Dog:
+	def __init__(self, name, color):
+		self.name=name
+		self.color = color
+	def bark(self):
+		print("woof!")
+fido = Dog("Fido", "brown")
+print(fido.color)
+fido.bark()
+# cool.bark() will do error
+print("-----------")
+class Student:
+	def __init__(self, name):
+		self.name=name
+	def saHi(self):
+		print("Hi from " + self.name)
+s1 = Student("Amy")
+s1.saHi()
+print("-----------")

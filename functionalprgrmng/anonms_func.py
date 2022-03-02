f48318a02954b55629489be80389042c7697dada
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-03-02 09:51:22 +0330

    Update anonms_func.py

--- functionalprgrmng/anonms_func.py
+++ functionalprgrmng/anonms_func.py
@@ -118,3 +118,11 @@ nums={0,1,2,3}&nums
 nums=filter(lambda x: x>1, nums)
 print(len(list(nums)))
 print("-------------")
+# power recursive
+def power(x, y):
+	if y ==0:
+		return 1
+	else:
+		return x*power(x, y-1)
+print(power(2, 3))
+print("-------------")

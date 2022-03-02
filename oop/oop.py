28a7d75942186d33c63f31f8975d29b82fc3bed2
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-03-02 16:31:34 +0330

    Update oop.py

--- oop/oop.py
+++ oop/oop.py
@@ -80,3 +80,16 @@ class B(A):
 B().spam()
 A().spam()
 print("-----------")
+# Magic Methods/ dunders like __init__
+# double underscore at end and beginnig
+class Vector2D:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+	def __add__(self, other):
+		return Vector2D(self.x + other.x, self.y+other.y)
+first = Vector2D(5, 7)
+second = Vector2D(3, 9)
+result= first + second
+print(result.x)
+print(result.y)

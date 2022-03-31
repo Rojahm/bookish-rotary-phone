2e8ac82735bde38b3b6a74d316b3233c27bca6d6
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-03-31 16:52:00 +0430

    Update easyexcersololearn.py

--- easyexcersololearn.py
+++ easyexcersololearn.py
@@ -48,3 +48,12 @@ if pesos * 0.02 < dollars:
 	print("Pesos")
 else:
 	print("Dollars")
+
+#Gotham City
+criminals = int(input())
+if criminals < 5:
+	print('I got this!')
+elif criminals >= 5 and criminals < 10:
+	print('Help me Batman')
+else:
+	print('Good Luck out there!')
